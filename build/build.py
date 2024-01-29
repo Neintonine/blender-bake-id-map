20458a8c5e5dbdc82fa6c9d8ab75d35592b7efa8
Michel Fedde <35878897+Neintonine@users.noreply.github.com>
2024-01-29 22:12:56 +0100

    improved build

--- build/build.py
+++ build/build.py
@@ -1,9 +1,11 @@
 import os
 import shutil
+import zipfile
 
 import versioning
 
-ZIP_FILE_NAME = "blender_bake-id-mask_{version}"
+ZIP_FILE_NAME = "blender_bake-id-mask_{version}.zip"
+PROJECT_NAME = "Bake ID Mask"
 
 CURRENT_PATH = os.path.dirname(os.path.realpath(__file__))
 
@@ -20,4 +22,19 @@ if __name__ == "__main__":
     versioning.save_version(nextVersion)
 
     filename = os.path.join(TARGET_PATH, ZIP_FILE_NAME.format(version=nextVersion.__str__()))
-    shutil.make_archive(filename, 'zip', SOURCE_PATH)
+    with zipfile.ZipFile(filename, 'w', zipfile.ZIP_DEFLATED) as zip:
+        for (root, dirs, files) in os.walk(SOURCE_PATH):
+            if '__pycache__' in dirs:
+                dirs.remove('__pycache__')
+
+            for file in files:
+
+                relativePath = os.path.relpath(
+                    os.path.join(root, file),
+                    SOURCE_PATH
+                )
+
+                zip.write(
+                    os.path.join(root, file),
+                    os.path.join(PROJECT_NAME, relativePath)
+                )
